1cb7744421d099edec772ff977f070fc18d682b8
Edmar dos Reis Silva <edmar.silva@sicoob.com.br>
2019-03-28 12:56:22 +0000

    After Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
-
-const orderSummary = (props) => {
-    const ingredientesSummary = Object.keys(props.ingredients)
-        .map(igKey =>{
-            return <li key={igKey} >
-                        <span 
-                            style={{textTransform: 'capitalize'}} >{igKey}
-                        </span>: {props.ingredients[igKey]} 
-                    </li>
-    });
+const orderSummary = ( props ) => {
+    const ingredientSummary = Object.keys( props.ingredients )
+        .map( igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li> );
+        } );
 
     return (
         <Aux>
             <h3>Your Order</h3>
-            <p>A delicios burger with the following ingredientes:</p>
+            <p>A delicious burger with the following ingredients:</p>
             <ul>
-                {ingredientesSummary}
+                {ingredientSummary}
             </ul>
-            <p>Continue Checkout ?</p>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     );
 };
