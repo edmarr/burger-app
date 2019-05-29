36de884b3ad4f8d7959c875d82c7b2d8a1956fdc
Edmar dos Reis Silva <edmar.silva@sicoob.com.br>
2019-05-29 16:12:04 +0000

    add sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrwer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Aux>
         <Toolbar />
+        <SideDrwer />
         <main className={classes.Content}>
             {props.children}
         </main>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () =>  (
   <ul>
-      <li><a href="/">A Link</a> </li>
+      <li className={classes.NavigationItems} >
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
+      </li>
 
   </ul>  
 );
